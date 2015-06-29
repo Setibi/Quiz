0aa8fda3f19f6f63665aed34fbe20673f89912fd
Setibi <setibi13@gmail.com>
2015-06-29 19:26:20 +0200

    Marco de la aplicacion

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var partials = require('express-partials');
+
 // importar enrutadores
 
 var routes = require('./routes/index');
@@ -20,6 +22,8 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(partials());
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
