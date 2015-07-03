6f04168556edc7c0f3516518019e62b281c7612d
Setibi <setibi13@gmail.com>
2015-07-03 17:32:46 +0200

    Lista de Preguntas

--- models/models.js
+++ models/models.js
@@ -33,14 +33,18 @@ exports.Quiz = Quiz; // exportar la definicion de la tabla Quiz
 
 // sequelize.sync() crea e inicializa tabla de preguntas en DB
 sequelize.sync().success(function(){
-        //success(..) ejecuta el manejador una vez creada la tabla
+        //then(..) ejecuta el manejador una vez creada la tabla
         Quiz.count().success(function (count){
                 if(count === 0){ // la tabla se inicializa solo si esta vacia
                     Quiz.create({
                         pregunta: 'Capital de Italia',
                         respuesta: 'Roma'
+                    });
+                    Quiz.create({
+                        pregunta: 'Capital de Portugal',
+                        respuesta: 'Lisboa'
                     })
-                        .success( function(){console.log('Base de datos inicializada')});
+                    .then( function(){console.log('Base de datos inicializada')});
                 };
             });
     });
