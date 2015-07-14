a5500814048f4abd9f94ce68747bf1d3910f5022
Setibi <setibi13@gmail.com>
2015-07-14 13:58:46 +0200

    Validación de entradas

--- app.js
+++ app.js
@@ -58,7 +58,8 @@ if (app.get('env') === 'development') {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
-      error: err
+      error: err,
+      errors: []
     });
   });
 }
@@ -71,7 +72,8 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
-    error: {}
+    error: {},
+    errors: []
   });
 });
 
