7485f6dbe550c93e0a08b24bf2ff498ace05e226
Setibi <setibi13@gmail.com>
2015-07-18 16:24:40 +0200

    Moderacion de Comentarios

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -1,5 +1,22 @@
 var models = require('../models/models.js');
 
+// Autoload :id de comentarios
+exports.load = function(req,res,next,commentId){
+    models.Comment.find({
+        where: {
+            id: Number(commentId)
+        }
+    }).then(function(comment){
+        if(comment){
+            req.comment = comment;
+            next();
+        }else{
+            next(new Error('No existe commentId='+commentId))
+        }
+    }).catch(function(error){next(error)});
+};
+
+
 // GET /quizes/:quizId/comments/new
 exports.new = function(req,res){
     res.render('comments/new.ejs', {quizid: req.params.quizId, errors: []});
@@ -12,8 +29,6 @@ exports.create = function(req,res){
 
     var errors = comment.validate();//ya qe el objeto errors no tiene then
 
-
-
          if (errors) {
              res.render('comments/new.ejs',
                  {comment: comment, quizid: req.params.quizId, errors: errors.errors});
@@ -25,8 +40,13 @@ exports.create = function(req,res){
                  })
              // res.redirect: Redireccion HTTP a lista de preguntas
          }
+};
 
+// GET /quizes/:quizId/comments/:commentId/publish
+exports.publish = function(req, res){
+    req.comment.publicado = true;
 
-
-
+    req.comment.save( {fields: ["publicado"]})
+        .then( function(){ res.redirect('/quizes/'+req.params.quizId);})
+        .catch( function(error){next(error)});
 };
